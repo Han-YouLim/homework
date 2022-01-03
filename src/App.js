fa4f2e7182f024cbd91d8b360f71508a6652d7ba
KICKER <gksdbfla12@naver.com>
2022-01-04 00:40:27 +0900

    [mod]

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import './App.css';
 import { createGlobalStyle } from 'styled-components';
-import Users from './Users';
-// import UserListTemplate from './component/UserListTemplate'
-// import UserList from './component/UserLIst'
+import styled from 'styled-components';
+import UserList from './component/UserLIst'
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -11,12 +10,28 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
+const UserListTemplateBlock= styled.div`
+width: 1024px;
+height: 1000px;
+position: relative; /* 추후 박스 하단에 추가 버튼을 위치시키기 위한 설정 */
+background: white;
+border-radius: 16px;
+box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.04);
+margin: 0 auto; /* 페이지 중앙에 나타나도록 설정 */
+margin-top: 96px;
+margin-bottom: 32px;
+display: flex;
+flex-direction: column;
+`;
+
 function App() { 
   return (
     <>
     <GlobalStyle />
-    <Users />
-    </>
+    <UserListTemplateBlock>
+     <UserList />  
+    </UserListTemplateBlock>
+   </>
   );
 }
 
--- src/component/UserLIst.js
+++ src/component/UserLIst.js
@@ -1,22 +1,77 @@
-import React, { useState, useEffect }from 'react';
+import React, { useReducer, createContext, useState, useEffect, useContext, useRef }from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
 
-import UserItem from './UserItem';
-
 const UserListBlock = styled.div`
   flex: 1; /*자신이 차지 할 수 있는 영역을 꽉 채우도록*/
-  padding: 20px 32px;
+  padding: 4px;
   padding-bottom: 48px;
   overflow-y: auto; /*내용이 넘칠때만 가로 스크롤바 표시*/
   /*background: gray; 사이즈 조정이 잘 되고 있는지 확인하기 위한 임시 스타일 */
 `;
 
-function UserList() {
-  return( 
-  <UserListBlock>
-    <UserItem text="프로젝트 생성하기" />
+const UserItemBlock = styled.div`
+  margin: 8px;
+  display: flex;
+  align-items: center;
+  border: 2px solid #ced4da;
+  padding-top: 12px;
+  padding-bottom: 12px;
+  &:hover {
+    background: #38d9a9;
+  }
+`;
 
+//item
+//width:(1024-24)//2=500
+//height:(1000-20)//5=176
+
+function UserList() {
+  const [users, setUsers] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    //github api에서 users GET
+    const fetchUsers = async () => {
+      try {
+        // 요청이 시작 할 때에는 error 와 users 를 초기화하고
+        setError(null);
+        setUsers(null);
+        // loading 상태를 true 로 바꿉니다.
+        setLoading(true);
+        const response = await axios.get(
+          'https://api.github.com/users?per_page=10'
+        );
+        setUsers(response.data); // 데이터는 response.data 안에 들어있습니다.
+      } catch (e) {
+        setError(e);
+      }
+      setLoading(false);
+    };
+
+    fetchUsers();
+}, []);
+if (loading) return <div>로딩중..</div>;
+if (error) return <div>에러가 발생했습니다</div>;
+if (!users) return null;
+return (
+  <UserListBlock>
+      {users.map(user => (
+        <UserItemBlock>
+          <h1>
+            {user.id}.
+          </h1>
+          &nbsp;&nbsp;&nbsp;
+          <h2>
+            {user.login}
+          </h2>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <p>
+            ({user.url})
+          </p>
+        </UserItemBlock> 
+      ))}
   </UserListBlock>
   );
 }
